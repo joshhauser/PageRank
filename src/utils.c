11d0213ce8f3bbb67559bb23ca2b1e54c02d9459
joshhauser <joshua.hauser@gmx.fr>
2022-01-02 22:28:31 +0100

    cleaned functions in src/utils.c

--- src/utils.c
+++ src/utils.c
@@ -13,12 +13,11 @@
  */
 int index_in_vertices_array(Vertice *array, int array_length, int searched) {
   int i = 0;
-  int index = -1;
 
   for (i = 0; i < array_length; i++)
     if (array[i].label == searched) return i;
 
-  return i != array_length ? index : -1;
+  return -1;
 }
 
 /**
@@ -33,10 +32,9 @@ int index_in_vertices_array(Vertice *array, int array_length, int searched) {
  */
 int index_in_int_array(int *array, int array_length, int searched) {
   int i = 0;
-  int index = -1;
 
   for (i = 0; i < array_length; i++)
     if (array[i] == searched) return i;
 
-  return i != array_length ? index : -1;
+  return -1;
 }
